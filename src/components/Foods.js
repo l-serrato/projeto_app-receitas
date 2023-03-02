d7d646dcfad249494f3bc63829bb12c6a69cfa3f
Luis <l-serrato@live.com>
2023-03-02 20:28:50 -0300

    [Grupo 12] -requisitos 2-6, Tela de Login

--- src/components/Foods.js
+++ src/components/Foods.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Foods() {
-  <h1>Criei só porque precisava para um teste do requisito 6</h1>;
+  <h1>Criei só porque precisava para um dos testes do requisito 6</h1>;
 }
 
 export default Foods;
