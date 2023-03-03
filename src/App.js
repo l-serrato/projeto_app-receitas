2f2da47d464dfcdc8b3ea4fea796b4aa079f753f
Heitor Soares <heytorpg7@gmail.com>
2023-03-03 05:28:35 -0300

    Rotas da aplicação adicionadas, Header condicional funcionando

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './pages/Login';
+import Meals from './pages/Meals';
+import Profile from './pages/Profile';
+import Drinks from './pages/Drinks';
+import Recipe from './components/Recipe';
+import DoneRecipes from './pages/DoneRecipes';
+import FavoriteRecipes from './pages/FavoriteRecipes';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Switch>
+      <Route exact path="/" render={ () => <Login /> } />
+      <Route exact path="/meals" render={ (props) => <Meals { ...props } /> } />
+      <Route exact path="/profile" render={ (props) => <Profile { ...props } /> } />
+      <Route exact path="/drinks" render={ (props) => <Drinks { ...props } /> } />
+      <Route
+        exact
+        path="/done-recipes"
+        render={ (props) => <DoneRecipes { ...props } /> }
+      />
+      <Route
+        exact
+        path="/favorite-recipes"
+        render={ (props) => <FavoriteRecipes { ...props } /> }
+      />
+      <Route
+        exact
+        path="/meals/:id-da-receita"
+        render={ (props) => <Recipe { ...props } /> }
+      />
+      <Route
+        exact
+        path="/drinks/:id-da-receita"
+        render={ (props) => <Recipe { ...props } /> }
+      />
+      <Route
+        exact
+        path="/meals/:id-da-receita/in-progress"
+        render={ (props) => <Recipe { ...props } /> }
+      />
+      <Route
+        exact
+        path="/drinks/:id-da-receita/in-progress"
+        render={ (props) => <Recipe { ...props } /> }
+      />
+
+    </Switch>
   );
 }
 
