5d7a0047eeb25f63d31611c7e473d5a0df4a36e3
biel-igl <gabrielpires524@gmail.com>
2023-03-07 18:12:46 -0300

    cria estados para receber os resultados do endpoit clicado

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -4,6 +4,8 @@ import { useHistory } from 'react-router-dom';
 export default function Categories() {
   const history = useHistory();
   const [categories, setcategories] = useState();
+  const [endpoint, setEndpoint] = useState();
+  const [resultsCategorie, setResultsCategorie] = useState();
   const fetchCategoriesMeals = async () => {
     try {
       const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
@@ -33,6 +35,30 @@ export default function Categories() {
     verificaBarra();
   }, [history]);
 
+  useEffect(() => {
+    const fetchResultsByCategorie = async () => {
+      try {
+        const response = await fetch(endpoint);
+        const json = await response.json();
+        setResultsCategorie(json);
+      } catch (error) {
+        throw new Error(error);
+      }
+    };
+    if (endpoint) {
+      fetchResultsByCategorie();
+    }
+  }, [endpoint]);
+
+  const categorieClick = ({ target }) => (
+    history.location.pathname === '/meals'
+      ? setEndpoint(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${target.value}`)
+      : setEndpoint(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${target.value}`));
+
+  const allButon = () => (
+    history.location.pathname === '/meals'
+      ? setEndpoint('https://www.themealdb.com/api/json/v1/1/search.php?s=') : setEndpoint('https://www.thecocktaildb.com/api/json/v1/1/search.php?s='));
+
   return (
     <div>
       {
@@ -41,11 +67,14 @@ export default function Categories() {
             <button
               key={ cada.strCategory }
               data-testid={ `${cada.strCategory}-category-filter` }
+              value={ cada.strCategory }
+              onClick={ categorieClick }
             >
               { cada.strCategory }
             </button>))
           : 'categorias'
       }
+      <button data-testid="All-category-filter" onClick={ allButon }>All</button>
     </div>
   );
 }
