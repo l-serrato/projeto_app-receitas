aa306569613f108efb5196c0ddd6096445d86264
biel-igl <gabrielpires524@gmail.com>
2023-03-08 17:28:28 -0300

    cria botoes de compartilhar e favoritar

---
+++ src/components/Buttons.js
@@ -0,0 +1,8 @@
+export default function Buttons() {
+  return (
+    <div>
+      <button data-testid="share-btn">compartilhar</button>
+      <button data-testid="favorite-btn">favoritar</button>
+    </div>
+  );
+}
