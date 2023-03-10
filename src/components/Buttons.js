265ab1d4df98fd00687a2ee7a2d5a9daa569763c
biel-igl <gabrielpires524@gmail.com>
2023-03-10 15:02:44 -0300

    adiciona Buttons.js

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,8 +1,34 @@
+import { useHistory } from 'react-router-dom';
+import copy from 'clipboard-copy';
+import { useState } from 'react';
+import Alert from 'react-bootstrap/Alert';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+
 export default function Buttons() {
+  const history = useHistory();
+  const [textCopy, setTextCopy] = useState(false);
+
+  const shareButton = () => {
+    const url = `http://localhost:3000${history.location.pathname}`;
+    copy(url);
+    setTextCopy(true);
+    const time = 5000;
+    setTimeout(() => setTextCopy(false), time);
+  };
+
   return (
     <div>
-      <button data-testid="share-btn">compartilhar</button>
-      <button data-testid="favorite-btn">favoritar</button>
+      <button data-testid="share-btn" onClick={ shareButton }>
+        <img alt="shareIcon" src={ shareIcon } />
+      </button>
+      <button data-testid="favorite-btn">
+        <img alt="heartIcon" src={ whiteHeartIcon } />
+      </button>
+      {textCopy && (
+        <Alert variant="success" onClose={ () => setTextCopy(false) } dismissible>
+          <Alert.Heading>Link copied!</Alert.Heading>
+        </Alert>)}
     </div>
   );
 }
